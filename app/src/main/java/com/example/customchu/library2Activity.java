e02d20c36323a5e1a700a24b665ebdfa54c31b38
Adriane James Jabagat Puzon <adrianejames03@gmail.com>
2023-11-08 16:23:47 +0800

    feat: library2activity

--- app/src/main/java/com/example/customchu/library2Activity.java
+++ app/src/main/java/com/example/customchu/library2Activity.java
@@ -10,6 +10,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.google.firebase.database.DataSnapshot;
@@ -20,6 +21,9 @@ import com.google.firebase.database.ValueEventListener;
 
 public class library2Activity extends AppCompatActivity {
 
+    ImageView lowCrowd;
+    ImageView midCrowd;
+    ImageView hiCrowd;
     ImageButton mapBack;
     Button btn1stFloor;
     int libRoom1, libRoom2;
@@ -37,20 +41,21 @@ public class library2Activity extends AppCompatActivity {
         });
 
         btn1stFloor = findViewById(R.id.btn1stFloor);
-        btn1stFloor.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(library2Activity.this, mapActivity.class);
-                startActivity(intent);
-            }
+        btn1stFloor.setOnClickListener(view -> {
+            Intent intent = new Intent(library2Activity.this, mapActivity.class);
+            startActivity(intent);
         });
 
         room1Count = findViewById(R.id.room1Count);
         room2Count = findViewById(R.id.room2Count);
 
+        lowCrowd = findViewById(R.id.imageView17);
+        midCrowd = findViewById(R.id.imageView16);
+        hiCrowd = findViewById(R.id.imageView9);
+
         databaseFacility = FirebaseDatabase.getInstance().getReference();
-        DatabaseReference room1 = databaseFacility.child("Room1").child("Current");
-        DatabaseReference room2 = databaseFacility.child("Room2").child("Current");
+        DatabaseReference room1 = databaseFacility.child("Rooms").child("GF").child("Current");
+        DatabaseReference room2 = databaseFacility.child("Rooms").child("2F").child("Current");
 
         ValueEventListener postListener = new ValueEventListener() {
             @Override
@@ -72,6 +77,25 @@ public class library2Activity extends AppCompatActivity {
             public void onDataChange(DataSnapshot dataSnapshot) {
                 room2Count.setText(dataSnapshot.getValue() + "");
                 libRoom2 = Integer.parseInt(dataSnapshot.getValue() + "");
+                if (libRoom2 < 26)
+                {
+                    lowCrowd.setVisibility(View.VISIBLE);
+                    midCrowd.setVisibility(View.INVISIBLE);
+                    hiCrowd.setVisibility(View.INVISIBLE);
+
+                }
+                else if (libRoom2 < 51)
+                {
+                    lowCrowd.setVisibility(View.INVISIBLE);
+                    midCrowd.setVisibility(View.VISIBLE);
+                    hiCrowd.setVisibility(View.INVISIBLE);
+                }
+                else
+                {
+                    lowCrowd.setVisibility(View.INVISIBLE);
+                    midCrowd.setVisibility(View.INVISIBLE);
+                    hiCrowd.setVisibility(View.VISIBLE);
+                }
             }
             @Override
             public void onCancelled(DatabaseError databaseError) {
